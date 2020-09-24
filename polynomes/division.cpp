6d9e234e8190629e7df73af7a6d1d4dfeabefe9d
VinceBro <vbreault99@gmail.com>
2020-09-23 23:53:39 -0400

    algo question 1 fonctionne mais pas parfait

--- polynomes/division.cpp
+++ polynomes/division.cpp
@@ -8,13 +8,43 @@ void division(const Polynome& dividende, // Le polynome qui est divise
 	      Polynome& reste) {	   // Parametre de sortie: le reste de la division
   assert(diviseur.degre() > 0 || diviseur.coefficient(0).numerateur() != 0); // Pas de division par zero
 
-  // INSEREZ VOTRE CODE ICI
+  Polynome reste_courant, dividende_intermediaire, terme_courant;
+  Polynome dividende_courant(dividende);
+  Rationnel coefficient_terme_courant;
+  int degre_diviseur = diviseur.degre();
+  int degre_dividende_courant = dividende_courant.degre();
+
+  while (degre_dividende_courant >= degre_diviseur) {
+
+      coefficient_terme_courant = dividende_courant.coefficient(degre_dividende_courant) / diviseur.coefficient(degre_diviseur);
+      Polynome terme_courant(coefficient_terme_courant, degre_dividende_courant - degre_diviseur);
+
+      dividende_intermediaire = terme_courant * diviseur;
+      reste_courant = dividende_courant - dividende_intermediaire;
+      dividende_courant = reste_courant;
+
+      degre_dividende_courant = dividende_courant.degre();
+
+      quotient = quotient + terme_courant;
+  }
+
+  reste = Polynome(reste_courant);
 }
 
 Polynome plus_grand_commun_diviseur(const Polynome& a, const Polynome& b) {
   assert(a >= b);
   assert(a.degre() > 0 || a.coefficient(0).numerateur() != 0);
 
-  // INSEREZ VOTRE CODE ICI
-  return Polynome();
+  //m = a, n = b
+
+  // implémenter le tour de passe passe pour améliorer l'éxécution? -> m mod n
+  Polynome qx, rx, ax(a), bx(b);
+
+  while (bx.degre() > 0) {
+    division(ax, bx, qx, rx);
+    ax = Polynome(bx);
+    bx = Polynome(rx);
+  }
+
+  return ax;
 }
--- polynomes/main.cpp
+++ polynomes/main.cpp
@@ -2,10 +2,21 @@
 #include "division.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Polynome ax, bx;
+  Polynome ax, bx, qx, rx;
 
+  ax = Polynome(Rationnel(1, 1), 6) + Polynome(Rationnel(5, 1), 5) + Polynome(Rationnel(4, 1), 4) + Polynome(Rationnel(2, 1), 0);
+  bx = Polynome(Rationnel(1, 1), 5) + Polynome(Rationnel(3, 1), 4) + Polynome(Rationnel(4, 1), 3);
+//  ax = Polynome(Rationnel(1, 1), 2) + Polynome(Rationnel(2, 1), 1) - Polynome(Rationnel(-1, 1), 0);
+//  bx = Polynome(Rationnel(1, 1), 2) + Polynome(Rationnel(-1, 1), 0);
 
-    plus_grand_commun_diviseur(ax, bx);
-    return 0;
+  std::cout << "ax : " << ax << std::endl;
+  std::cout << "bx : " << bx << std::endl;
+
+  division(ax, bx, qx, rx);
+
+  std::cout << "qx : " << qx << std::endl;
+  std::cout << "rx : " << rx << std::endl;
+  std::cout << "pgcd : " << plus_grand_commun_diviseur(ax, bx) << std::endl;
+  return 0;
 }
+
